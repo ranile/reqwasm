e5f67c0eebdbade436327a7bf623696a50254b53
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-11-17 21:38:20 +0300

    feat: feature gate json support

--- src/error.rs
+++ src/error.rs
@@ -9,6 +9,7 @@ pub enum Error {
     #[error("{0}")]
     JsError(JsError),
     /// Error returned by `serde` during deserialization.
+    #[cfg(feature = "json")]
     #[error("{0}")]
     SerdeError(
         #[source]
