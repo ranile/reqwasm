e3e7cb8b963e6f344e25807a7c3fcdc30585672b
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-03-11 15:46:09 +0500

    re-export everything from gloo-net

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,6 @@
 //! HTTP requests library for WASM apps. It provides idiomatic Rust bindings for the `web_sys`
 //! `fetch` and `WebSocket` API.
 //!
-//! See module level documentation for [`http`] and [`websocket`] to learn more.
+//! This re-exports _all_ the API from [gloo_net] crate.
 
-#![deny(
-    missing_docs,
-    missing_debug_implementations,
-    missing_copy_implementations
-)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
-
-mod error;
-#[cfg(feature = "http")]
-#[cfg_attr(docsrs, doc(cfg(feature = "http")))]
-pub mod http;
-#[cfg(feature = "websocket")]
-#[cfg_attr(docsrs, doc(cfg(feature = "websocket")))]
-pub mod websocket;
-
-pub use error::*;
+pub use gloo_net::*;
